93a5f0f26d048294ce506c6c568401e0805a70de
Mathis027 <62936675+Mathis027@users.noreply.github.com>
2022-08-02 22:38:09 +0200

    jkhwef

--- assets/includes/connect.php
+++ assets/includes/connect.php
@@ -100,3 +100,37 @@ function refreshAdminUserData($id, $email ,$name, $is_admin, $password) {
     }
 
 }
+
+    if(!isset($_SESSION['id']) && isset($_COOKIE['identifier']) && isset($_COOKIE['securitytoken'])) {
+        $identifier = $_COOKIE['identifier'];
+        $securitytoken = $_COOKIE['securitytoken'];
+
+        $statement = geteinkaufUsersDB()->prepare("SELECT * FROM securitytokens WHERE identifier = :identifier");
+        $result = $statement->execute([
+                "identifier" => $identifier,
+            ]
+        );
+        $securitytoken_row = $statement->fetch();
+
+        if($securitytoken !== $securitytoken_row['securitytoken']) {
+            exit;
+
+        } else { //Token war korrekt
+            //Setze neuen Token
+            function random_string()
+            {
+                $bytes = random_bytes(16);
+                $str = bin2hex($bytes);
+                return $str;
+            }
+            $neuer_securitytoken = random_string();
+            $insert = geteinkaufUsersDB()->prepare("UPDATE securitytokens SET securitytoken = :securitytoken WHERE identifier = :identifier");
+            $insert->execute(array('securitytoken' => sha1($neuer_securitytoken), 'identifier' => $identifier));
+            setcookie("identifier",$identifier,time()+(3600*24*365)); //1 Jahr Gültigkeit
+            setcookie("securitytoken",$neuer_securitytoken,time()+(3600*24*365)); //1 Jahr Gültigkeit
+
+            //Logge den Benutzer ein
+            $_SESSION['id'] = $securitytoken_row['user_id'];
+            header("Refresh:0");
+        }
+}
