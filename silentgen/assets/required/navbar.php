78fd32766d8b9080880428ad726ecb94b98d77cc
Mathis027 <62936675+Mathis027@users.noreply.github.com>
2022-08-18 20:43:03 +0100

    Sollet gehen

---
+++ silentgen/assets/required/navbar.php
@@ -0,0 +1,51 @@
+<!-- Navbar -->
+<body style="background: linear-gradient(to bottom, #1e5799 0%,#97b9d5 100%);">
+
+<nav class="navbar navbar-expand-lg blur top-0 z-index-3 shadow position-absolute py-2 start-0 end-0 ">
+    <div class="container-fluid">
+        <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="../pages/dashboard.html">
+            Silent-GEN
+        </a>
+        <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon mt-2">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+              </span>
+        </button>
+        <div class="collapse navbar-collapse" id="navigation">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link d-flex align-items-center me-2 active" aria-current="page" href="?type=Minecraft">
+                        <img src="assets/img/minecraft.png" class="me-1" alt="">
+                        Minecraft
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link me-2" href="?type=Disney%2B">
+                        <i class="fa fa-user opacity-6 text-dark me-1"></i>
+                        Disney
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link me-2" href="?type=Netflix">
+                        <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
+                        Netflix
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link me-2" href="?type=Crunchyroll">
+                        <i class="fas fa-key opacity-6 text-dark me-1"></i>
+                        Crunchyroll
+                    </a>
+                </li>
+            </ul>
+            <ul class="navbar-nav d-lg-block d-none">
+                <li class="nav-item">
+                    <a href="https://discord.com" target="_blank" class="btn btn-sm mb-0 me-1 btn-primary"><i class="fa-brands fa-discord me-1"></i> Discord</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- End Navbar -->
--- silentgen/index.php
+++ silentgen/index.php
@@ -318,7 +318,7 @@ if(isset($_GET["type"])) {
 <!-- Github buttons -->
 <script async defer src="https://buttons.github.io/buttons.js"></script>
 <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-<script src="../assets/js/argon-dashboard.min.js?v=2.0.4"></script>
+<script src="../assets/js/argon-dashboard.js?v=2.0.4"></script>
 </body>
 
 </html>
