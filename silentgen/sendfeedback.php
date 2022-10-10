53c355026c431e66e3c737fb1079cfc06e3fcfbe
Mathis Kräkel <mkraekel@seibert-media.net>
2022-10-10 17:39:06 +0200

    hioehf

--- silentgen/sendfeedback.php
+++ silentgen/sendfeedback.php
@@ -1,10 +1,11 @@
 <?php
 require "assets/includes/connect.php";
-if(isset($_POST["feedback"])) {
+if(isset($_GET["feedback"])) {
         $empfaenger = "mathis@kraekel.com";
         $betreff = "Feedback";
         $from = "no-reply@silent-gen.com";
-        $text = "Feedback: <br> " . $_POST["feedback"];
+        $text = "Feedback: <br> " . $_GET["feedback"];
 
         mail($empfaenger, $betreff, $text, $from);
+        echo "Email gesendet";
 }
