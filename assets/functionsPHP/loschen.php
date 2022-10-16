adfd7ca8691eb014036f20ad831fab95557a9dd8
Mathis Kräkel <mkraekel@seibert-media.net>
2022-10-16 15:59:31 +0200

    kfwkelh

--- assets/functionsPHP/loschen.php
+++ assets/functionsPHP/loschen.php
@@ -2,11 +2,14 @@
 session_start();
 require "../includes/connect.php";
 function listeLoeschen() {
-    $list = $_POST["liststring"];
-    $einkaufdb = geteinkaufDB();
-    $einkaufdb->query("DROP TABLE $list ");
-    $listdata = geteinkaufUsersDB();
-    $listdata->query("DELETE FROM listdata WHERE liststring = $list ");
-    echo $list;
+
+    // aus einkaufDB löschen
+        $list = $_POST["liststring"];
+        $einkaufdb = geteinkaufDB();
+        $einkaufdb->query("DROP TABLE $list ");
+        $listdata = geteinkaufUsersDB();
+        $listdata->query("DELETE FROM listdata WHERE liststring = '$list' ");
+        header("Location: ../../list/mylists.php");
+        // aus UserDB löschen
 }
 listeLoeschen();
