ea2121035eaef0ba232cf84a6af1fa341109ef85
Your Name <you@example.com>
2022-10-18 21:13:41 +0200

    oiefwh

--- silentgen/generator.php
+++ silentgen/generator.php
@@ -78,7 +78,7 @@ $lastupdated = lastUpdated($dbname);
                                     ?>
                                 </div>
 
-                            <!-- Werbung -->
+                            <!-- Werbung
 
                             <script async="async" data-cfasync="false" src="//marketingabsentremembered.com/3e39aa7a16dd360f2df372a3909e2a65/invoke.js"></script>
                             <div style="position:absolute; left: 10px; height: 400px; width: 200px " id="container-3e39aa7a16dd360f2df372a3909e2a65"></div>
@@ -93,7 +93,7 @@ $lastupdated = lastUpdated($dbname);
                                     };
                                     document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://marketingabsentremembered.com/acca7a8549d727daf0347bdc665a9e0e/invoke.js"></scr' + 'ipt>');
                                 </script>
-                            </div>
+                            </div> -->
                                 <div class="card-footer ">
                                     <div class="d-flex align-items-center justify-content-end " >
                                         <div class="col-12 col-lg-4 col-md-6">
