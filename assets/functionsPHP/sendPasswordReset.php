e4dfadbec9729d9c75bc04c569a83d509c46b396
Mathis Kräkel <mkraekel@seibert-media.net>
2022-10-23 18:25:53 +0200

    generator fix blab la

--- assets/functionsPHP/sendPasswordReset.php
+++ assets/functionsPHP/sendPasswordReset.php
@@ -26,5 +26,5 @@ if(isset($_POST["reset-email"])) {
     $msg = "Bitte klicke auf diesen Link um dein Passwort zurückzusetzen $link";
     mail($email, "Password Reset", $msg, $headers);
     echo "email versendet";
-    header("Location: ../../generator.php");
+    header("Location: ../../index.php");
 }
