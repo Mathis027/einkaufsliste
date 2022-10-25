578b53e39f612b8d4260ee7cab63847f73cc10aa
Mathis Kräkel <mkraekel@seibert-media.net>
2022-10-25 22:49:43 +0200

    silent-gen Änderungen

--- silentgen/generated.php
+++ silentgen/generated.php
@@ -71,10 +71,10 @@ $gen_other = $other[1];
                                 <h6>What do you think of our service?</h6>
                                 <form action="https://accounts.silent-gen.com/sendfeedback.php" METHOD="post">
                                     <label for="discordtag">Discord tag</label>
-                                    <input name="discordtag" placeholder="SIlENT-GEN#3820" type="text" class="form-control w-50">
+                                    <input required name="discordtag" placeholder="SIlENT-GEN#3820" type="text" class="form-control w-50">
                                     <br>
                                     <label for="text">What can we do better?</label>
-                                    <textarea name="feedback" class="form-control mb-2" placeholder="Make a good design a$$"></textarea>
+                                    <textarea required name="feedback" class="form-control mb-2" placeholder="Make a good design a$$"></textarea>
 
                                     <input type="hidden" value="hfuiewhiuh29834zthuerfn34985uhrtg" name="token">
                                     <button type="submit" class="btn btn-success" value="absenden">Send</button>
